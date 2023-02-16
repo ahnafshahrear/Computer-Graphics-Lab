37d32c39e1e25707ec61ff9a8e799930c3c1b0a3
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2023-02-16 09:33:55 +0600

    National Flag added

---
+++ NationalFlag.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#include<graphics.h>
+
+using namespace std;
+
+int main()
+{
+    int gd, gm;
+    detectgraph(&gd, &gm);
+    initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
+
+    // Green Rectangle
+    setcolor(GREEN);
+    rectangle(50, 50, 350, 230);
+    setfillstyle(SOLID_FILL, GREEN);
+    floodfill(51, 51, GREEN);
+
+    // Red Rectangle
+    setcolor(RED);
+    circle(195, 140, 60);
+    setfillstyle(SOLID_FILL, RED);
+    floodfill(201, 141, RED);
+
+    // White Stand
+    setcolor(WHITE);
+    rectangle(40, 40, 50, 430);
+    setfillstyle(SOLID_FILL, WHITE);
+    floodfill(41, 41, WHITE);
+
+
+    getchar();
+    return 0;
+}
