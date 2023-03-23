acf5b5c20e1abcc2807402c35e1955d0092df7bd
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2023-03-24 03:29:10 +0600

    2D Scaling added

--- Readme.md
+++ Readme.md
@@ -1,12 +1,11 @@
 # Computer Graphics
 ### 3rd Year 2nd Semester - Computer Science & Engineering, University of Rajshahi
+### Course Code: CSE3222
 
 ## Author
-- Ahnaf Shahrear Khan - [@ahnafshahrear](https://github.com/ahnafshahrear)
-- Gmail - ahnafshahrearkhan@gmail.com
-
-## Language
-- C++
+- **Ahnaf Shahrear Khan, Computer Science & Engineering, University of Rajshahi**
+- **GitHub: [@ahnafshahrear](https://github.com/ahnafshahrear)**
+- **Gmail: ahnafshahrearkhan@gmail.com**
 
 ## CodeBlocks setup instructions
 - Download **TDM GCC 32 Bit Compiler** from this link: https://jmeubank.github.io/tdm-gcc/
@@ -19,7 +18,7 @@
 - Go to "Linker Settigns" -> Other linker options & add **"-lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -lole32"**
 
 ## Index
-- Draw the national flag of Bangladesh
+- Draw the national flag of Bangladesh ✓
 - Hidden surface elimination / visual surface ditection
 - 2D Translation, Rotation & Scaling
 - Create a Sphere, Elipsoid & Torus
---
+++ two-dimentional-scaling.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <graphics.h>
+
+using namespace std;
+
+int gd = DETECT, gm;
+int size;
+int x[10], y[10];
+double sfx, sfy;
+
+void draw()
+{
+    for (int i = 0; i < size; i++)
+    {
+        line(x[i], y[i], x[(i + 1) % size], y[(i + 1) % size]);
+    }
+}
+
+void scale()
+{
+    for (int i = 0; i < size; i++)
+    {
+        x[i] = x[i] * sfx;
+        y[i] = y[i] * sfy;
+    }
+}
+
+int main()
+{
+    cout << "Enter no. of sides in polygon: ";
+    cin >> size;
+    cout << "Enter each vertex coordinates:\n";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> x[i] >> y[i];
+    }
+    cout << "Enter scale factors(sfx & sfy):";
+    cin >> sfx >> sfy;
+    initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
+    setcolor(WHITE);
+    draw();
+    scale();
+    setcolor(RED);
+    draw();
+    getch();
+    return 0;
+}
+
+/*
+Input Format:
+
+Enter no. of sides in polygon: 4
+Enter each vertex coordinates:
+1 1
+1 100
+100 100
+100 1
+Enter scale factors(sfx & sfy):3 2
+*/
+
