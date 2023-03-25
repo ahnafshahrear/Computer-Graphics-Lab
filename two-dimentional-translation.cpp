870f52070155823b6a9cf5deafc99556958e44b4
abdullah16-blip <64699480+abdullah16-blip@users.noreply.github.com>
2023-03-25 14:39:26 +0600

    Update two-dimentional-translation.cpp
    Hello. I want to contribute to your code, if that's okay. I know you because we live in the same building. Your room is diagonal to mine to be precise. Hence diagonally, I am proposing a change.

--- two-dimentional-translation.cpp
+++ two-dimentional-translation.cpp
@@ -4,21 +4,21 @@
 using namespace std;
 
 int gd = DETECT, gm;
-int size;
+int polypoint;  // previously this variable was named as 'size', but the line 'using namespace std' implies an error which says, 'size is ambiguous', this is because there is already a function named size() in std. So we changed it to 'polypoint'
 int x[10], y[10];
 int tx, ty;
 
 void draw()
 {
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < polypoint; i++)
     {
-        line(x[i], y[i], x[(i + 1) % size], y[(i + 1) % size]);
+        line(x[i], y[i], x[(i + 1) % polypoint], y[(i + 1) % polypoint]);
     }
 }
 
 void translate()
 {
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < polypoint; i++)
     {
         x[i] += tx;
         y[i] += ty;
@@ -28,27 +28,27 @@ void translate()
 int main()
 {
     cout << "Enter no. of sides in polygon: ";
-    cin >> size;
+    cin >> polypoint;
     cout << "Enter each vertex coordinates:\n";
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < polypoint; i++)
     {
         cin >> x[i] >> y[i];
     }
     cout << "Enter translation factors(tx & ty): ";
     cin >> tx >> ty;
-    initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
+    initgraph(&gd, &gm, (char *)"");
     setcolor(RED);
     draw();
     translate();
     setcolor(WHITE);
     draw();
     getch();
+    closegraph();
     return 0;
 }
 
 /*
 Input Format:
-
 Enter no. of sides in polygon: 4
 Enter each vertex coordinates:
 1 1
@@ -57,4 +57,3 @@ Enter each vertex coordinates:
 100 1
 Enter translation factors(sfx & sfy):150 50
 */
-
