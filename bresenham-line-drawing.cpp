542d7e5018819c33c818c87921e6800010dfdf4c
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2023-03-26 16:44:42 +0600

    Bresenhum Circle Drawing & Line Drawing updated

--- bresenham-line-drawing.cpp
+++ bresenham-line-drawing.cpp
@@ -10,14 +10,13 @@ void drawLine(int x1, int y1, int x2, int y2)
         swap(x1, x2);
         swap(y1, y2);
     }
-    int dx = x2 - x1, dy = abs(y1 - y2);
-    int p = 2 * dy - dx;
+    int dx = x2 - x1, dy = abs(y1 - y2), p = 2 * dy - dx;
     for (int x = x1, y = y1; x <= x2; x++)
     {
         putpixel(x, y, WHITE);
         if (p >= 0)
         {
-            y++;
+            (y1 < y2) ? y++ : y--;
             p += 2 * (dy - dx);
         }
         else p += 2 * dy;
@@ -26,14 +25,9 @@ void drawLine(int x1, int y1, int x2, int y2)
 
 int main()
 {
-    int x1, y1, x2, y2;
-    cout << "Enter the first coordinate: ";
-    cin >> x1 >> y1;
-    cout << "Enter the second coordinate: ";
-    cin >> x2 >> y2;
-    int gd = DETECT;
-    int gm = DETECT;
+    int gd = DETECT, gm = DETECT;
     initgraph(&gd, &gm, "");
+    int x1 = 100, y1 = 100, x2 = 300, y2 = 400;
     drawLine(x1, y1, x2, y2);
     getch();
     return 0;
