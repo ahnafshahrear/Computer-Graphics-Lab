8afc1f8dd0e4fd52344908c3e4ffdbfa684eb704
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2023-07-09 08:32:17 +0600

    two-dimensional-translation updated

--- two-dimentional-translation.cpp
+++ two-dimentional-translation.cpp
@@ -3,16 +3,18 @@
 
 using namespace std;
 
-void draw(vector<int> x, vector<int> y, int n)
+void draw(vector<int> x, vector<int> y)
 {
+    int n = x.size();
     for (int i = 0; i < n; i++)
     {
         line(x[i], y[i], x[(i + 1) % n], y[(i + 1) % n]);
     }
 }
 
-void translate(vector<int> &x, vector<int> &y, int n, int tx, int ty)
+void translate(vector<int> &x, vector<int> &y, int tx, int ty)
 {
+    int n = x.size();
     for (int i = 0; i < n; i++)
     {
         x[i] += tx;
@@ -24,7 +26,7 @@ int main()
 {
     int n; //... Number of vertex of the polygon
     cin >> n;
-    vector<int> x(n), y(n); //... (x,y) coordinates of vertex
+    vector<int> x(n), y(n); //... (x,y) coordinates of polygon vertex points
     for (int i = 0; i < n; i++)
     {
         cin >> x[i] >> y[i];
@@ -36,10 +38,10 @@ int main()
     initgraph(&gd, &gm, "");
 
     setcolor(WHITE);
-    draw(x, y, n); //... The polygon before translation
-    translate(x, y, n, tx, ty); //... Applying 2d translation
+    draw(x, y); //... The polygon before translation
+    translate(x, y, tx, ty); //... Applying 2d translation
     setcolor(YELLOW);
-    draw(x, y, n); //... The polygon after translation
+    draw(x, y); //... The polygon after translation
 
     getch();
     closegraph();
@@ -49,10 +51,6 @@ int main()
 /*//... Sample Input-Output:
 ___________________________________________________________________________________________________________________________________________________________________________________________________________________________
 Input:
-3
-100 100
-100 200
-200 100
-100 100
+4 100 100 100 200 200 200 200 100 150 150
 
 *///...
