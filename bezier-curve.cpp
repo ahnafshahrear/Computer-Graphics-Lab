0b67228cef880a9e6bdd79d637fab1fe8d921442
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2023-07-09 14:49:52 +0600

    bezier-curve updated

--- bezier-curve.cpp
+++ bezier-curve.cpp
@@ -22,9 +22,9 @@ double bezierFunction(int k, int n, double u)
     return nCr(n, k) * pow(u, k) * pow((1 - u), (n - k));
 }
 
-void bezierCurve(vector<pair<int, int>> xy)
+void bezierCurve(vector<pair<int, int>> points)
 {
-    int n = xy.size() - 1;
+    int n = points.size() - 1;
     double eps = 0.0001;
     for (double u = 0; u <= 1; u += eps)
     {
@@ -32,14 +32,13 @@ void bezierCurve(vector<pair<int, int>> xy)
         for (int k = 0; k <= n; k++)
         {
             double bez = bezierFunction(k, n, u);
-            x += xy[k].first * bez;
-            y += xy[k].second * bez;
+            x += points[k].first * bez;
+            y += points[k].second * bez;
         }
         putpixel(x, y, WHITE);
     }
-    for (auto x: xy)
+    for (auto x: points)
     {
-        delay(1000);
         putpixel(x.first, x.second, WHITE);
         circle(x.first, x.second, 5);
     }
@@ -50,8 +49,8 @@ int main()
     int gd = DETECT, gm = DETECT;
     initgraph(&gd, &gm, "");
 
-    vector<pair<int, int>> xy = {{27, 243}, {101, 47}, {324, 197}, {437, 23}};
-    bezierCurve(xy);
+    vector<pair<int, int>> points = {{27, 243}, {101, 47}, {324, 197}, {437, 23}};
+    bezierCurve(points);
 
     getch();
     closegraph();
